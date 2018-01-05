8ab330220837011c4e97f9f973c298e58ecb68eb
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-04 16:03:53 -0800

    Addition and subtraction functionality added to calculator

---
+++ calculator.py
@@ -0,0 +1,31 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+# Your code goes here
+while True:
+    user_input = raw_input("> ")
+    user_input = user_input.split()
+
+    operator = user_input[0]
+
+    if len(user_input) == 2:
+        num1 = float(user_input[1])
+    elif len(user_input) == 3:
+        num1 = float(user_input[1])
+        num2 = float(user_input[2])
+
+    if operator == 'q':
+        break
+    else:
+        if operator == '+':
+            answer = add(num1, num2)
+            print answer
+        elif operator == '-':
+            answer = subtract(num1, num2)
+            print answer
